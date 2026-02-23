40caf49d550b947dce56c19779c7c7a92a516e57
Atanu Maity <atanuamit2004@gmail.com>
2026-02-23 20:39:35 +0530

    Implement QR code generation and saving
    Added functionality to save QR code image to a specified file path.

--- QR code.py
+++ QR code.py
@@ -1,2 +1,11 @@
+# pip install qrcode[pil]
+
 import qrcode
 url = input ("enter the url:").strip()
+file_path =  "C:\\Users\\LENOVO\\OneDrive\\Pictures\\qrcode.png"
+
+qr = qrcode. QRCode()
+qr.add_data(url)
+
+img = qr.make_image()
+image.save(file_path)
