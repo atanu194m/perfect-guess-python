ab915a84e6695620e6b66067dca24e8e52bca6b7
Atanu Maity <atanuamit2004@gmail.com>
2026-04-18 19:51:11 +0530

    Refactor QR code generation and save process

--- QR code.py
+++ QR code.py
@@ -1,11 +1,6 @@
 # pip install qrcode[pil]
 
 import qrcode
-url = input ("enter the url:").strip()
-file_path =  "C:\\Users\\LENOVO\\OneDrive\\Pictures\\qrcode.png"
-
-qr = qrcode. QRCode()
-qr.add_data(url)
-
-img = qr.make_image()
-image.save(file_path)
+qr= qrcode.make("add url")
+qr.save("My_qrcode.png")
+print("qrcode generated successfully.")
